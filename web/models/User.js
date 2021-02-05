67957a73f106c051e5b8ba23d8055d1f40ee32c4
somamizobuchi <soma.mizo@gmail.com>
2021-02-05 11:21:23 -0500

    minor

--- web/models/User.js
+++ web/models/User.js
@@ -1,15 +1,18 @@
 const mongoose = require('mongoose')
-const bcrypt = require('bcrypt')
 
 const UserSchema = mongoose.Schema({
 	email: {
 		type: String,
 		unique: true,
+		trim: true,
+		lowercase: true
 	},
 	password: String
 }, {
-	timestamps: { createdAt: 'created_at' }
+	timestamps: {
+		createdAt: 'created_at',
+		updatedAt: 'updated_at'
+	}
 })
-
 
 module.exports = mongoose.model('User', UserSchema)
