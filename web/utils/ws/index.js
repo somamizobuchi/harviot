3025e087361dd15f2fd2f1a43aac0729ee9c30b6
somamizobuchi <soma.mizo@gmail.com>
2021-02-08 04:24:26 -0500

    ws port changed to 8080

--- web/utils/ws/index.js
+++ web/utils/ws/index.js
@@ -7,7 +7,7 @@ module.exports = (server) => {
 	// Store all client sockets
 	const sockets = {}
 	// Create new WebSocketServer
-	const wss = new WebSocket.Server({ port: 80 })
+	const wss = new WebSocket.Server({ port: 8080 })
 
 	server.on('upgrade', (request, socket, head) => {
 		console.log(head)
